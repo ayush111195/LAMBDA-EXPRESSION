fe21dfabbbd54c0c8fb94fed5db5ff681e6b6465
Ayush Gupta <121301901+ayush111195@users.noreply.github.com>
2023-08-27 00:27:41 +0530

    Update Loop.java

--- LambdaExpression/src/Work/Loop.java
+++ LambdaExpression/src/Work/Loop.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Loop {
     public static void main(String[] args) {
-        List<String> name=new ArrayList< String>();
+        List<String> name=new ArrayList<String>();
         name.add("ayush");
         name.add("arpit");
         name.add("shivam");
